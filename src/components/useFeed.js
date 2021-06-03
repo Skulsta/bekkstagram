cfbe235b8a84fa5e5c53b0cd0b2eb1fe9ab3c23a
skulsta <sti.sku@gmail.com>
2021-06-03 09:47:37 +0200

    Fetching posts from a server

---
+++ src/components/useFeed.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { getFeed } from "../server";
+
+const useFeed = () => {
+  const [images, setImages] = useState(null);
+
+  useEffect(() => {
+    getFeed().then((data) => setImages(data));
+  }, []);
+  return images;
+};
+
+export default useFeed;
--- src/views/FeedPage.js
+++ src/views/FeedPage.js
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Image from "../components/Image";
-import images from "../data/images";
 import Post from "../components/Post";
 import useTitle from "../components/useTitle";
+import useFeed from "../components/useFeed";
 
 const FeedPage = () => {
   useTitle("🎨 Bekkstagram");
+
+  const images = useFeed();
+
   return (
     <React.Fragment>
       <div className="posts">
-        {images.map((image) => {
-          return (
-            <Post
-              author={image.username}
-              timestamp={image.createdDate}
-              key={image.id}
-            >
-              <Link to={`/post/${image.id}`}>
-                <Image src={image.url} alt={image.description} />
-              </Link>
-            </Post>
-          );
-        })}
+        {!images ? (
+          <div>Loading...</div>
+        ) : (
+          images.map((image) => {
+            return (
+              <Post
+                author={image.username}
+                timestamp={image.createdDate}
+                key={image.id}
+              >
+                <Link to={`/post/${image.id}`}>
+                  <Image src={image.url} alt={image.description} />
+                </Link>
+              </Post>
+            );
+          })
+        )}
       </div>
     </React.Fragment>
   );
