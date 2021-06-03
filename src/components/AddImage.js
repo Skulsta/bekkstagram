ab50277805971a77a8c414e7eec250c29c1f150e
skulsta <sti.sku@gmail.com>
2021-06-03 10:39:35 +0200

    Added a modal for uploading images

--- src/components/AddImage.js
+++ src/components/AddImage.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaCameraRetro } from "react-icons/fa";
+import { Dialog } from "@reach/dialog";
+import VisuallyHidden from "@reach/visually-hidden";
+import "@reach/dialog/styles.css";
 
 const AddImage = () => {
+  const [showDialog, setShowDialog] = useState(false);
+  const open = () => setShowDialog(true);
+  const close = () => setShowDialog(false);
+
   return (
-    <button className="camera-button">
-      <FaCameraRetro />
-    </button>
+    <div>
+      <button onClick={open} className="camera-button">
+        <FaCameraRetro />
+      </button>
+      <Dialog isOpen={showDialog} onDismiss={close}>
+        <button className="close-button" onClick={close}>
+          <VisuallyHidden>Close</VisuallyHidden>
+          <span aria-hidden>x</span>
+        </button>
+        <p>Some content here</p>
+      </Dialog>
+    </div>
   );
 };
 
---
+++ src/components/Modal.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Dialog } from "@reach/dialog";
+import "@reach/dialog/styles.css";
+
+const Modal = () => {
+  return <Dialog />;
+};
+
+export default Modal;
