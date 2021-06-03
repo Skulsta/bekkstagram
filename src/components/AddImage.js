f1d24ad1b9901825264a405b1c913d836b9b22f3
skulsta <sti.sku@gmail.com>
2021-06-03 12:45:26 +0200

    Modal styling

--- src/components/AddImage.js
+++ src/components/AddImage.js
@@ -5,7 +5,7 @@ import VisuallyHidden from "@reach/visually-hidden";
 import "@reach/dialog/styles.css";
 
 const AddImage = () => {
-  const [showDialog, setShowDialog] = useState(false);
+  const [showDialog, setShowDialog] = useState(true);
   const open = () => setShowDialog(true);
   const close = () => setShowDialog(false);
 
@@ -14,24 +14,16 @@ const AddImage = () => {
       <button onClick={open} className="camera-button">
         <FaCameraRetro />
       </button>
-      <Dialog isOpen={showDialog} onDismiss={close}>
-        <button className="close-button" onClick={close}>
-          <VisuallyHidden>Close</VisuallyHidden>
-          <span aria-hidden>x</span>
-        </button>
+      <Dialog
+        className="add-image-dialog"
+        isOpen={showDialog}
+        onDismiss={close}
+      >
         <h2>Last opp bilde</h2>
-        <form className="image-form">
-          <div>
-            <label htmlFor="url">URL</label>
-            <br />
-            <input type="text" name="url" />
-          </div>
-          <div>
-            <label htmlFor="description">Beskrivelse</label>
-            <br />
-            <textarea name="description" rows="3" />
-          </div>
-        </form>
+        <input className="add-image-dialog-input" />
+        <textarea className="add-image-dialog-input" rows="5" />
+        <button type="submit">Post</button>
+        <button>Avbryt</button>
       </Dialog>
     </div>
   );
